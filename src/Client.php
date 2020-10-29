65c8ae6c524d7f24ed3fc11f85316b934c99fed2
Tustin <Tustin@users.noreply.github.com>
2020-10-28 22:36:04 -0500

    implement new psn auth method

--- src/Client.php
+++ src/Client.php
@@ -2,26 +2,27 @@
 
 namespace PlayStation;
 
-use PlayStation\Api\MessageThread;
-use PlayStation\Api\User;
 use PlayStation\Api\Game;
-use PlayStation\Api\Community;
-
-use PlayStation\Http\HttpClient;
-use PlayStation\Http\ResponseParser;
-use PlayStation\Http\TokenMiddleware;
-
+use PlayStation\Api\User;
 use GuzzleHttp\Middleware;
 use GuzzleHttp\Psr7\Response;
 
+use PlayStation\Api\Community;
+use PlayStation\Http\HttpClient;
+use PlayStation\Api\MessageThread;
+
+use PlayStation\Http\ResponseParser;
+use PlayStation\Http\TokenMiddleware;
+
 class Client {
 
-    const AUTH_API      = 'https://auth.api.sonyentertainmentnetwork.com/2.0/';
+    const AUTH_API = 'https://ca.account.sony.com/api/';
+    const BASE_URL = 'https://m.np.playstation.net/api/';
 
-    private const CLIENT_ID     = 'ebee17ac-99fd-487c-9b1e-18ef50c39ab5';
-    private const CLIENT_SECRET = 'e4Ru_s*LrL4_B2BD';
-    private const DUID          = '0000000d00040080027BC1C3FBB84112BFC9A4300A78E96A';
-    private const SCOPE         = 'kamaji:get_players_met kamaji:get_account_hash kamaji:activity_feed_submit_feed_story kamaji:activity_feed_internal_feed_submit_story kamaji:activity_feed_get_news_feed kamaji:communities kamaji:game_list kamaji:ugc:distributor oauth:manage_device_usercodes psn:sceapp user:account.profile.get user:account.attributes.validate user:account.settings.privacy.get kamaji:activity_feed_set_feed_privacy kamaji:satchel kamaji:satchel_delete user:account.profile.update';
+    private const CLIENT_ID = '8c52bc6a-4ad1-43fb-bd63-4465cf818937';
+    private const CLIENT_SECRET = 'bKC6jEYJ6CCXdxzr';
+    private const DUID = '0000000d00040080027BC1C3FBB84112BFC9A4300A78E96A';
+    private const SCOPE = 'openid:age openid:content_ctrl kamaji:get_privacy_settings kamaji:get_account_hash openid:user_id openid:ctry_code openid:lang';
 
     private $httpClient;
     private $onlineId;
@@ -34,7 +35,9 @@ class Client {
 
     public function __construct(array $options = [])
     {
-        $this->options = $options;
+        $this->options = array_merge([
+            'base_uri' => self::AUTH_API
+        ], $options);
         $this->httpClient = new HttpClient(new \GuzzleHttp\Client($this->options));
     }
     
@@ -47,6 +50,8 @@ class Client {
      */
     public function login(string $ticketUuidOrRefreshToken, string $code = null) 
     {
+        // @TEMP
+        throw new \Exception("broken");
         if ($code === null) {
             $response = $this->httpClient()->post(self::AUTH_API . 'oauth/token', [
                 "app_context" => "inapp_ios",
@@ -122,7 +127,10 @@ class Client {
             )
         );
 
-        $newOptions = array_merge(['handler' => $handler], $this->options);
+        $newOptions = array_merge([
+            'handler' => $handler,
+            'base_uri' => self::BASE_URL
+        ], $this->options);
     
         return new HttpClient(new \GuzzleHttp\Client($newOptions));
     }
@@ -130,13 +138,73 @@ class Client {
 
     public function loginWithNpsso(string $npsso)
     {
-        $response = $this->httpClient()->post(self::AUTH_API . 'oauth/token', [
-            'client_id' => self::CLIENT_ID,
-            'client_secret' => self::CLIENT_SECRET,
-            'scope' => self::SCOPE,
-            'grant_type' => 'sso_cookie',
-        ], HttpClient::FORM, [
+        // With the PS App revamp, we now need a JWT token.
+        $response = $this->httpClient()->get('authz/v3/oauth/authorize', [
+            'access_type' => 'offline',
+            'app_context' => 'inapp_ios',
+            'auth_ver' => 'v3',
+            'cid' => '60351282-8C5F-4D5E-9033-E48FEA973E11',
+            'client_id' => 'ac8d161a-d966-4728-b0ea-ffec22f69edc',
+            'darkmode' => 'true',
+            'device_base_font_size' => 10,
+            'device_profile' => 'mobile',
+            'duid' => '0000000d0004008088347AA0C79542D3B656EBB51CE3EBE1',
+            'elements_visibility' => 'no_aclink',
+            'extraQueryParams' => '{
+                PlatformPrivacyWs1 = minimal;
+            }',
+            'no_captcha' => 'true',
+            'redirect_uri' => 'com.playstation.PlayStationApp://redirect',
+            'response_type' => 'code',
+            'scope' => 'psn:mobile.v1 psn:clientapp',
+            'service_entity' => 'urn:service-entity:psn',
+            'service_logo' => 'ps',
+            'smcid' => 'psapp:settings-entrance',
+            'support_scheme' => 'sneiprls',
+            'token_format' => 'jwt',
+            'ui' => 'pr',
+        ], [
             'Cookie' => 'npsso=' . $npsso
+        ]);
+        
+        if ($response->getStatusCode() !== 302) {
+            // TODO: Throw proper exception here.
+            throw new \Exception('Invalid response code from oauth/autorize.');
+        }
+
+        $location = $response->getHeader('Location');
+
+        if (!$location) {
+            throw new \Exception('Missing redirect location from oauth/authorize.');
+        }
+
+        parse_str(parse_url($location[0], PHP_URL_QUERY), $params);
+
+        if (!array_key_exists('code', $params)) {
+            throw new \Exception('Missing code from oauth/authorize.');
+        }
+
+        $response = $this->httpClient()->post('authz/v3/oauth/token', [
+            'smcid' => 'psapp%3Asettings-entrance',
+            'access_type' => 'offline',
+            'code' => $params['code'],
+            'service_logo' => 'ps',
+            'ui' => 'pr',
+            'elements_visibility' => 'no_aclink',
+            'redirect_uri' => 'com.playstation.PlayStationApp://redirect',
+            'support_scheme' => 'sneiprls',
+            'grant_type' => 'authorization_code',
+            'darkmode' => 'true',
+            'device_base_font_size' => 10,
+            'device_profile' => 'mobile',
+            'app_context' => 'inapp_ios',
+            'extraQueryParams' => '{
+                PlatformPrivacyWs1 = minimal;
+            }',
+            'token_format' => 'jwt'
+        ], HttpClient::FORM, [
+            'Cookie' => 'npsso=' . $npsso,
+            'Authorization' => 'Basic YWM4ZDE2MWEtZDk2Ni00NzI4LWIwZWEtZmZlYzIyZjY5ZWRjOkRFaXhFcVhYQ2RYZHdqMHY=',
         ]);
 
         $this->accessToken = $response->access_token;
@@ -247,6 +315,11 @@ class Client {
     public function user(string $onlineId = '') : User
     {
         return new User($this, $onlineId);
+    }
+
+    public function aa()
+    {
+        $this->httpClient()->get('https://m.np.playstation.net/api/userProfile/v1/internal/users/profiles?accountIds=290499036638149769%2C6302737558727256776%2C5767969705352314860%2C8660297613407809012%2C3318601581187974976%2C1794949560635596299%2C4468193837129027570%2C937218632316989458%2C5695843137456681910%2C7230702695231133866%2C2673037779524917826%2C7794101426778942609%2C3863143976691624727%2C3387758873256323321%2C7200968299130045846%2C643742757001727694%2C7927222913481535097%2C7886051232230562055%2C1220975848120722090%2C3169237629135083655%2C2813495812580870637%2C7919012979851873890%2C7068032807127798573%2C4959981001468776598%2C2805295645697401011%2C2964358804368618665%2C4645077389662723469%2C5794783855171231964%2C7900415681776679368%2C2486997276581965084%2C2493426535140808664%2C9080896438515493000%2C7130601423182633922%2C3912091914131564086%2C4320405660322087082%2C2196492594985375697%2C4355907905257225891%2C4659648722389483350%2C7839997113793959515%2C1021019581734755711%2C8914630277991330655%2C714356860632959838%2C771197688321275726%2C5439800386292523989%2C461813134570437919%2C8757422912664509555%2C6761352691515275753%2C6374239285526955388%2C585828778556322229%2C2379992714711872512%2C4367308563909230524%2C53223675607332110%2C1343596725589745841%2C1075970181868891095%2C1589844052109037392%2C2038382839787618096%2C6190288666262892979%2C8838352505110018759%2C7821507023663338445%2C7798383085517380875%2C548843795526398268%2C3404056621793690129%2C2211100396049156590%2C2029064944159430032%2C6804785205228014029%2C9036049092293166783%2C7959627026491411880%2C5606864473888784809%2C1865326376723044044%2C6236990150345940032%2C1751272709830908318%2C7124328791336006328%2C2979333742140583485%2C7872826187695676502%2C3808346567713520103%2C4598840736712551038%2C617023336643399668%2C4289768539348637785%2C6037662901497980231%2C8123693936931797193%2C6182351992776041204%2C6991911350856527680%2C573016432886960867%2C368118438435212941%2C5768080933006748975%2C8303463348201408600%2C4100202504455308276%2C7878355682701390793%2C6555173597669780588%2C2366605971145003267%2C4485389378206716984%2C7995893745498737045%2C7318283285218908995%2C7092186148169881433%2C13637444542984362%2C3948047011464002841%2C3142186831889868855%2C8186815934450134368%2C4923378550674279772%2C6342687153230660965');
     }
 
     /**
